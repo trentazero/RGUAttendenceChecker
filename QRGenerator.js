d9112bf6817f3a41952c351a6c1abca0df91c614
HydraOpt <a.gadaleta@rgu.ac.uk>
2017-04-15 13:59:48 +0100

    removed alert

--- QRGenerator.js
+++ QRGenerator.js
@@ -3,11 +3,13 @@ var qrcode = new QRCode("qrcode");
 function makeCode () {
     var elText = document.getElementById("text");
 
+    /*
     if (!elText.value) {
         alert("Input a text");
         elText.focus();
         return;
     }
+    */
 
     qrcode.makeCode(elText.value);
 }
