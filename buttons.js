dc18be371fddd3feb6bc6d48c0ae992e8b2cb6da
HydraOpt <a.gadaleta@rgu.ac.uk>
2017-04-15 20:53:20 +0100

    close?

--- buttons.js
+++ buttons.js
@@ -17,6 +17,6 @@ $(function(){
 function addStudent() {
 	var newStud = {id:$("#ID").val(), name:$("#studName").val(), presence: false};
 	console.log(newStud);
-	$("#dialog").dialog("close");
+	$(this).dialog('close');
 
 }
