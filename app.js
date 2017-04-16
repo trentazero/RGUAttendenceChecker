eeaa247738a29c9f25d4b4121684ee2fb5955a3a
HydraOpt <a.gadaleta@rgu.ac.uk>
2017-04-16 08:44:16 +0100

    zzz

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ var app = new Vue({
         if (student.id == content){
           student.presence = "[V]";
           studCounter++;
+          console.log(studCounter);
         }
       });
     });
